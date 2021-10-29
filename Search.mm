9b246fb5cbc5e312fc4f478261e18e579b00402c
unknown <suravidey100@gmail.com>
2021-10-29 12:36:43 +0530

    mm , tcs and rtm added

---
+++ Search.mm
@@ -0,0 +1,18 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1635423244135" ID="ID_273585569" MODIFIED="1635423266682" TEXT="Search">
+<node CREATED="1635423278305" ID="ID_1306174099" MODIFIED="1635424112194" POSITION="right" STYLE="bubble" TEXT="Requirements">
+<node CREATED="1635423358712" ID="ID_1887850777" MODIFIED="1635424112194" TEXT="SERC_R_1 Search option embeded in main menu">
+<node CREATED="1635423442435" ID="ID_1006301610" MODIFIED="1635424112194" TEXT="searching an item with valid name">
+<node CREATED="1635423556491" ID="ID_1112613403" MODIFIED="1635424112195" TEXT="enter some trending item name and verify search success"/>
+<node CREATED="1635423608521" ID="ID_1421750054" MODIFIED="1635424112195" TEXT=""/>
+</node>
+<node CREATED="1635423514494" ID="ID_1709702248" MODIFIED="1635424112195" TEXT="serching an item which is invald/not listed in application"/>
+</node>
+<node CREATED="1635423643242" ID="ID_4369729" MODIFIED="1635424112195" TEXT="SERC_R_2 Fileters for sorting and top filters and total results "/>
+</node>
+<node CREATED="1635423286825" ID="ID_484018212" MODIFIED="1635423292301" POSITION="left" TEXT="Actors"/>
+<node CREATED="1635423298524" ID="ID_1879427037" MODIFIED="1635423298524" POSITION="right" TEXT=""/>
+<node CREATED="1635423306764" ID="ID_10490976" MODIFIED="1635423326705" POSITION="left" TEXT="Modules"/>
+</node>
+</map>
